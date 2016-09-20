d339f7e1c2e9feedbebb5e3cb4af4d5fb9741a8a
DXkite <DXkite@163.com>
2016-09-20 22:38:46 +0800

    编译字符串

--- mini/core/View/Compiler/Pomelo.php
+++ mini/core/View/Compiler/Pomelo.php
@@ -11,15 +11,39 @@ class View_Compile_Pomelo
     // 编译单文件
     public function compileFile(string $file, array $include_path=[])
     {
+        if(Storage::exist($file))
+        {
+            
+        }
+        return false;
     }
     // 编译多文件
     public function compileFiles(array $files, array $include_path=[])
     {
+        
     }
     // 编译文本
-    public function compileText(string $file, array $include_path=[])
+    public function compileText(string $text, array $include_path=[])
     {
-        
+        $result='';
+        foreach (token_get_all($value) as $token) {
+            if (is_array($token)) {
+                list($tag, $content) = $token;
+                // 所有将要编译的文本
+                if ($tag == T_INLINE_HTML) {
+                    $content=self::compileString($content,$include_path);
+                }
+                $result .=$content;
+            } else {
+                $result .=$token;
+            }
+        }
+        return $result;
+    }
+
+    private function compileString(string $str,array $include_path=[])
+    {
+
     }
     // 错误报错
     public function error()
