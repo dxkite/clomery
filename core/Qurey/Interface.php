c5b73e99cd97e835819674d8b268f6e418506215
DXkite <DXkite@163.com>
2016-09-22 22:42:57 +0800

    修正数据库接口i

--- core/Qurey/Interface.php
+++ core/Qurey/Interface.php
@@ -1,8 +1,13 @@
 <?php
-
+// 数据库通用接口
 interface Qurey_Interface{
     const FETCH_ASSOC=PDO::FETCH_ASSOC;
     const FETCH_BOTH=PDO::FETCH_BOTH;
     const FETCH_NUM=PDO::FETCH_NUM;
     function __construct(string $qurey,array $binds=[]);
+    public function fetch(int $fetch_style = self::FETCH_ASSOC);
+    public function fetchAll(int $fetch_style = self::FETCH_ASSOC);
+    public function values(array $values);
+    public function error();
+    public function erron();
 }
