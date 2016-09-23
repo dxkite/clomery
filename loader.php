3bc919147401ed9ced69a6e6a5bd4dd3e6d3dce8
DXkite <DXkite@163.com>
2016-09-23 09:35:19 +0800

    修改测试,独立出Visit

--- loader.php
+++ loader.php
@@ -15,27 +15,9 @@
     // 配置文件名
     defined('APP_CONF') or define('APP_CONF', '.conf');
     defined('WEB_MIME') or define('WEB_MIME', '.mime');
+    defined('APP_VISIT') or define('APP_VISIT', '.visit.php');
     
     require_once CORE_PATH.'/Core.php';
-
     View::loadCompile();
-
-    (new Page_Controller(function ($id, $name) {
-        echo 'OK ==> ', $id, $name;
-    }))-> url('/{id}/{name}')->with('id', 'int')->with('name', 'string');
-
-    
-
-    Page::visit('/getUser/{id}', function ($id=0) {
-        var_dump(Page::url('main', ['id'=>5, 'name'=>'urlpage']));
-        return (new Qurey('SELECT * FROM `#{users}` WHERE `uid`=:uid LIMIT 1;', ['uid'=>$id]))->fetch();
-    })
-    ->with('id', 'int')
-    ->json();
-
-    Page::default(function ($page) {
-        View::set('hello', '404 - No  Find');
-        echo '__default__';
-    });
-    Page::autoload('/admin', ['/admin']);
+    require_once APP_ROOT.'/'.APP_VISIT;
     Page::display();
