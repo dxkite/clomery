3cd7183242ef2af7695e2d4b4c8d0e45fc1edf65
DXkite <DXkite@163.com>
2016-09-28 23:00:14 +0800

    添加样式表测试

--- res/view/head.pml.php
+++ res/view/head.pml.php
@@ -3,4 +3,5 @@
 <head>
 	<title><?php Env::echo(isset($title) ? $title : 'Three - 三人行，必有我师焉') ?></title>
 	<?php Env::include("page_meta") -> render(); ?>
+	<link rel="stylesheet" href="main.css">
 </head>
