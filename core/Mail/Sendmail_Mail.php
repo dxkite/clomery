d555b8cd7c5fd7d78589eca9a08f298ba5c872a9
DXkite <DXkite@163.com>
2016-09-30 07:12:46 +0800

    添加忽略

---
+++ core/Mail/Sendmail_Mail.php
@@ -0,0 +1,10 @@
+<?php
+
+// set_error_handler(array($this, '_handleMailErrors'));
+//             $result = mail(
+//                 $this->recipients,
+//                 $this->_mail->getSubject(),
+//                 $this->body,
+//                 $this->header,
+//                 $this->parameters);
+//             restore_error_handler();
