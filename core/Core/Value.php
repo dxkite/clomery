25d8027ec1fa13b2be0cdc581aead7c43c26634f
DXkite <DXkite@163.com>
2016-10-03 09:57:21 +0800

    Var注释添加

--- core/Core/Value.php
+++ core/Core/Value.php
@@ -1,32 +1,81 @@
 <?php
 namespace Core;
+
+/**
+ * Class Value
+ * @package Core
+ */
 class Value
 {
-    protected  $var;
+    /**
+     * @var
+     */
+    protected $var;
+
+    /**
+     * Value constructor.
+     * @param $var
+     */
     public function __construct($var)
     {
         $this->var=$var;
     }
-    public function __get(string $name)
+
+    /**
+     * @return mixed
+     */
+    public function getVar()
     {
-        return isset($this->var[$name])?$this->var[$name]:'undefined:'.$name;
+        return $this->var;
     }
 
+    /**
+     * @param mixed $var
+     */
+    public function setVar($var)
+    {
+        $this->var = $var;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function __get(string $name)
+    {
+        return isset($this->var[$name])?$this->var[$name]:'U:['.$name.']';
+    }
+
+    /**
+     * @param string $name
+     * @param $value
+     * @return mixed
+     */
     public function __set(string $name, $value)
     {
         return $this->var[$name]=$value;
     }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
     public function __isset(string $name)
     {
         return isset($this->var[$name]);
     }
-    public function __call(string $name,$args)
+
+    /**
+     * @param string $name
+     * @param $args
+     * @return mixed|string
+     */
+    public function __call(string $name, $args)
     {
-        $fmt=isset($this->var[$name])?$this->var[$name]:isset($args[0])?$args[0]:'undefined:'.$name;
-        if (count($args)>1)
-        {
+        $fmt=isset($this->var[$name])?$this->var[$name]:(isset($args[0])?$args[0]:'U:['.$name.']');
+        if (count($args)>1) {
             $args[0]=$fmt;
-            return call_user_func_array('sprintf',$args);
+            return call_user_func_array('sprintf', $args);
         }
         return $fmt;
     }
