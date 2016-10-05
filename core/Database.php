c132714bf558d5ab724956149271c92741aff7bc
DXkite <DXkite@163.com>
2016-10-05 15:59:50 +0800

    待完善的数据库导出，导入

---
+++ core/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+class Database
+{
+    public static function backup(string $output)
+    {
+    }
+    public static function import(string $input)
+    {
+    }
+    public static function import_sql(string $input_sql)
+    {
+    }
+}
