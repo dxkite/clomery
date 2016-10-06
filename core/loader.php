d31027a89473a13869f0256c29db9b85b7222f0f
DXkite <DXkite@163.com>
2016-10-06 16:54:01 +0800

    系统独立前准备

--- core/loader.php
+++ core/loader.php
@@ -1,5 +1,5 @@
 <?php
-    
+
     defined('DOC_ROOT') or define('DOC_ROOT', __DIR__.'/..');
     defined('WEB_ROOT') or define('WEB_ROOT', DOC_ROOT.'/public');
     
@@ -10,7 +10,7 @@
     // APP资源目录
     defined('APP_RES')or define('APP_RES', DOC_ROOT.'/res');
     // 语言文件夹
-    defined('APP_LANG')or define('APP_LANG',APP_RES.'/lang');
+    defined('APP_LANG')or define('APP_LANG', APP_RES.'/lang');
     // 视图控制
     defined('APP_VIEW')or define('APP_VIEW', APP_RES.'/view');
     defined('APP_TPL')or define('APP_TPL', APP_RES.'/tpl');
@@ -21,29 +21,34 @@
     // 载入内置函数 PS:就是个自动加载，和配置加载
     require_once CORE_PATH.'/Core.php';
     // 设置PHP属性
-    set_time_limit(conf('timelimit',0));
+    set_time_limit(conf('timelimit', 0));
     // 设置时区
-    date_default_timezone_set(conf('timezone','PRC'));
+    date_default_timezone_set(conf('timezone', 'PRC'));
+    // 系统开始
+    Event::shift('System_Boot', true)->call();
+
     // 载入页面编译器 -> |  后续添加钩子函数后添加到系统钩子
-    View::loadCompile(); 
+    View::loadCompile();
     // 载入页面URL配置规则
     require_once APP_ROOT.'/'.APP_VISIT;
-    Event::shift('System_Boot',true)->call();
     // Debug 模式 实时生成模板
-    if (conf('DEBUG',0)==1) {
+    if (conf('DEBUG', 0)==1) {
         // View::theme('spider');
         View::compileAll();
-        
-    } 
+    }
     // 开启Session
     Session::start();
     // 获取网站设置
     Site\Options::init();
     // 语言支持
-    Page::language(Cookie::get('lang','zh_cn'));
+    Page::language(Cookie::get('lang', 'zh_cn'));
     View::theme(Site\Options::getTheme());
+    Event::pop('System_Before_Display')->call();
     // 显示页面
     Page::display();
+    Event::pop('System_After_Display')->call();
+    // 系统结束
+    Event::pop('System_Exit')->call();    
     // 写入Cookie
     Cookie::write();
     // 回收过期缓存
