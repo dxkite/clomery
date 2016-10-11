470d8a7c52299df5432fdb094a424502ca23ced8
DXkite <DXkite@163.com>
2016-10-11 19:03:20 +0800

    文件上传标签修改

--- app/user/upload_file_test.php
+++ app/user/upload_file_test.php
@@ -1,15 +1,15 @@
-<?php  if (!isset($_FILES['userfile'])): ?>
+<?php  if (!isset($_FILES['upload'])): ?>
 <form enctype="multipart/form-data" action="/user/upload_file_test" method="POST">
-    Send this file: <input name="userfile" type="file" />
+    Send this file: <input name="upload" type="file" />
     <input type="submit" value="Send File" />
-    <input type="radio" id="public_1" name="public" value="1" checked="checked" >
+    <input type="radio" id="public_1" name="public" value="1" checked="checked" />
     <label for="public_1">公开</label>
-    <input type="radio" id="public_0" name="public" value="0">
+    <input type="radio" id="public_0" name="public" value="0" />
     <label for="public_0">私有</label>
 </form>
-<?else:
+<?php else:
 if ($info=UManager::hasSignIn()){
-    var_dump(Upload::uploadFile('userfile',$info['uid'],isset($_POST['public'])?$_POST['public']:1));
+    var_dump(Upload::uploadFile('upload',$info['uid'],isset($_POST['public'])?$_POST['public']:1));
 }
 else{
     Page::redirect('/user/SignIn');
