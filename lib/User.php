3c9b3cb14e90f687cd2531895bb35725f776c58a
DXkite <DXkite@163.com>
2016-10-29 15:54:54 +0800

    添加信息

--- lib/User.php
+++ lib/User.php
@@ -7,6 +7,7 @@ class User
     private $base=null;
     private $permission=null;
     private $history=null;
+    private $info=null;
 
     public function hasSignin()
     {
@@ -17,7 +18,7 @@ class User
             return false;
         }
     }
-    
+
     // 获取信息
     public function __get(string $name)
     {
@@ -43,5 +44,12 @@ class User
             $this->history=Common_User::getSigninLogs($this->base['uid']);
         }
         return $this->history;
+    }
+    public function info()
+    {
+        if (is_null($this->info)) {
+            $this->info=Common_User::getInfo($this->base['uid']);
+        }
+        return $this->info;
     }
 }
