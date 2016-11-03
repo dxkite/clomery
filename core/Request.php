332d80a6fcfa5f0742d41fc4a0a606f4a6f50198
DXkite <DXkite@163.com>
2016-11-03 14:45:12 +0800

    Requrst-post

--- core/Request.php
+++ core/Request.php
@@ -4,22 +4,35 @@ use Core\Value;
 class Request extends Core\Value
 {
     private static $_get=null;
+    private static $_post=null;
 
     public static function json()
     {
         $str=file_get_contents('php://input');
         return Core\JSON::decode($str, true);
     }
-    
+
     public static function get(string $name='')
     {
         if (is_null(self::$_get)) {
-            self::$_get=Value($_GET);
+            self::$_get=new Value($_GET);
         }
         if (is_null($name)) {
             return self::$_get;
         } else {
             return self::$_get->$name;
+        }
+    }
+
+    public static function post(string $name='')
+    {
+        if (is_null(self::$_POST)) {
+            self::$_post=new Value($_POST);
+        }
+        if (is_null($name)) {
+            return self::$_post;
+        } else {
+            return self::$_post->$name;
         }
     }
 
@@ -40,6 +53,7 @@ class Request extends Core\Value
         }
         return $ip;
     }
+
     public function ipAddress($ip)
     {
         $url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
