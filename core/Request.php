eab276f4ade30e5777439ca24724011237cd0e55
DXkite <DXkite@163.com>
2016-11-03 14:43:26 +0800

    Request添加GET

--- core/Request.php
+++ core/Request.php
@@ -1,13 +1,28 @@
 <?php
+use Core\Value;
 
 class Request extends Core\Value
 {
+    private static $_get=null;
+
     public static function json()
     {
         $str=file_get_contents('php://input');
         return Core\JSON::decode($str, true);
     }
     
+    public static function get(string $name='')
+    {
+        if (is_null(self::$_get)) {
+            self::$_get=Value($_GET);
+        }
+        if (is_null($name)) {
+            return self::$_get;
+        } else {
+            return self::$_get->$name;
+        }
+    }
+
     public static function ip()
     {
         if (getenv('HTTP_CLIENT_IP')) {
@@ -28,7 +43,7 @@ class Request extends Core\Value
     public function ipAddress($ip)
     {
         $url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
-        $ip=json_decode(@file_get_contents($url),true);
+        $ip=json_decode(@file_get_contents($url), true);
         return $ip;
     }
 }
