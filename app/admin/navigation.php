e743203e1e884c822524ce7e45722d3aa9843a0a
DXkite <DXkite@163.com>
2016-11-06 15:46:56 +0800

    独立式管理抽出

--- app/admin/navigation.php
+++ app/admin/navigation.php
@@ -1,13 +1,4 @@
 <?php
-if (isset($_POST['nav_set']))
-{
-    foreach($_POST['nav_set'] as $id=> $set)
-    {
-        Common_Navigation::update($id,$set);
-        
-    }
-    echo '设置成功';
-    header('Location:'.$_SERVER['PHP_SELF']);
-}
+
 Page::set('navigations',Common_Navigation::getNavset());
 Page::use('admin/navigation');
