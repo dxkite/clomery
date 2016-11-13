7add9bd52334661c22202b16ded8faebddad0987
DXkite <DXkite@163.com>
2016-11-13 12:08:00 +0800

    Caller实时输出

--- tools/caller.php
+++ tools/caller.php
@@ -6,16 +6,9 @@ $params=array_slice($argv, 2);
 if (isset($argv[1])) {
     print "\033[36mcall \033[34m".$argv[1]."(\033[32m".implode(',', $params)."\033[34m)\033[0m\r\n";
     print "\033[33m-----------------------------------\033[0m\r\n";
-    ob_start();
+    print "\033[33m# Function echo\033[0m\r\n";
+    print "\033[33m-----------------------------------\033[0m\r\n";
     $return=(new Core\Caller($argv[1]))->call($params);
-    $output=ob_get_clean();
-    if ($output) {
-        print "\033[33m# Function echo\033[0m\r\n";
-        print "\033[33m-----------------------------------\033[0m\r\n";
-        echo $output;
-        print "\r\n\r\n";
-    }
-    
     print "\033[33m# return value\033[0m\r\n";
     print "\033[33m-----------------------------------\033[0m\r\n";
     print_r($return);
