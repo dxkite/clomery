36162f7689ba0fe65a8a7a400c510e010b58673f
DXkite <DXkite@163.com>
2016-11-28 17:28:36 +0800

    发送通知接口OK

--- library/model/Notification.php
+++ library/model/Notification.php
@@ -7,6 +7,7 @@ class Notification
 {
     // system
     const TYPE_SYSTEM=1;        // 系统消息
+    // notify
     const TYPE_NOTIFY=2;        // 公告
     // send
     const TYPE_REPLY=3;         // 用户回复
@@ -18,21 +19,33 @@ class Notification
     const STATE_DELETE=2;       // 删除
 
     // 发送系统消息
-    public function system(string $message, $group_id=null,$type=self::TYPE_SYSTEM)
+    public function system(string $message, $group_id=null)
+    {
+        return self::broadcast(0,$message,$group_id,self::TYPE_SYSTEM);
+    }
+
+    // 发送通知消息
+    public function notify(int $send,string $message, $group_id=null)
+    {
+        return self::broadcast(0,$message,$group_id,self::TYPE_NOTIFY);
+    }
+
+    // 发送广播
+    protected function broadcast(int $send,string $message, $group_id=null,int $type)
     {
         try {
             Query::begin();
             $data=Query::insert('notification_data', ['data'=>$message]);
             if (is_null($group_id)) {
-                $id=Query::insert('notification', '(`recv`,`type`,`time`,`state`,`data`) SELECT `id`,:type,:time,:state,:data FROM `#{user}` WHERE 1;', ['data'=>$data, 'time'=>time(), 'type'=>self::TYPE_SYSTEM, 'state'=>self::STATE_UNREADE]);
+                $id=Query::insert('notification', '(`send`,`recv`,`type`,`time`,`state`,`data`) SELECT :send,`id`,:type,:time,:state,:data FROM `#{user}` WHERE 1;', ['send'=>$send,'data'=>$data, 'time'=>time(), 'type'=>$type, 'state'=>self::STATE_UNREAD]);
             } else {
                 $array=$group_id;
                 if (is_numeric($group_id) || is_string($group_id)) {
                     $array=[intval($group_id)];
                 }
                 $infield=Query::prepareIn('group_id', $array);
-                $param=array_merge(['data'=>$data, 'time'=>time(), 'type'=>$type, 'state'=>self::STATE_UNREADE], $infield['param']);
-                $id=Query::insert('notification', '(`recv`,`type`,`time`,`state`,`data`) SELECT `id`,:type,:time,:state,:data FROM `#{user}` WHERE '.$infield['sql'].';', $param);
+                $param=array_merge(['send'=>$send,'data'=>$data, 'time'=>time(), 'type'=>$type , 'state'=>self::STATE_UNREAD], $infield['param']);
+                $id=Query::insert('notification', '(`send`,`recv`,`type`,`time`,`state`,`data`) SELECT :send,`id`,:type,:time,:state,:data FROM `#{user}` WHERE '.$infield['sql'].';', $param);
             }
             Query::commit();
         } catch (\Exception $e) {
@@ -48,7 +61,7 @@ class Notification
         try {
             Query::begin();
             $data=Query::insert('notification_data', ['data'=>$message]);
-            $id=Query::insert('notification', ['send'=>$send, 'recv'=>$recv, 'data'=>$data, 'time'=>time(), 'type'=>$type, 'state'=>self::STATE_UNREADE]);
+            $id=Query::insert('notification', ['send'=>$send, 'recv'=>$recv, 'data'=>$data, 'time'=>time(), 'type'=>$type, 'state'=>self::STATE_UNREAD]);
             Query::commit();
         } catch (\Exception $e) {
             Query::rollBack();
