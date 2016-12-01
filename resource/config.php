28149c0cae7ed7737723904d3d3a77100cbd514c
DXkite <DXkite@163.com>
2016-12-01 13:58:07 +0800

    数据配置

---
+++ resource/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'db'=>[
+        'dbname'=>'test_hello',
+        'user'=>'root',
+        'passwd'=>'root',
+        'charset'=>'utf8',
+        'host'=>'localhost',
+    ],
+];
