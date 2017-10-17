e3882a814a19f2a9bd2aed648ef973b28efc5ad0
DXkite <dxkite@163.com>
2017-10-17 22:20:22 +0800

    更新：框架和模块

--- app/modules/dxkite-user/share/cn/atd3/user/Install.php
+++ app/modules/dxkite-user/share/cn/atd3/user/Install.php
@@ -6,10 +6,14 @@ class Install {
     public static function create(){
         $permissions='a:4:{i:0;s:11:"admin:pages";i:1;s:13:"admin:website";i:2;s:10:"admin:user";i:3;s:11:"admin:group";}';
         $user=new UserDAO;
+        // var_dump($user->export(TEMP_DIR.'/database1.txt'));
+        $user->truncate();
+        // var_dump($user->import(TEMP_DIR.'/database.txt'));
         // 创建管理员 邮箱 密码
         $user->add('dxkite','dxkite@qq.com','dxkite',1);
         // 创建分组
         $group=new GroupDAO;
+        $group->truncate();
         $group->insert(['id'=>1,'name'=>'超级管理员','sort'=>0,'permissions'=>$permissions]);
     }
 }
