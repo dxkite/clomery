80fb6b772c510b458bd81ed8aaf3097248534540
DXkite <dxkite@163.com>
2017-12-29 14:57:12 +0800

    添加：调试用户模拟

--- app/modules/dxkite-user/share/cn/atd3/user/User.php
+++ app/modules/dxkite-user/share/cn/atd3/user/User.php
@@ -7,20 +7,29 @@ use cn\atd3\visitor\Context;
 
 class User extends Visitor
 {
+    const  simulateUserToken='_simulate_user_id';
+    protected $simulate=null;
     // 检查是否是登陆状态
     protected function check(int $id, string $token)
     {
         $check=Manager::checkTokenVaild($id, $token);
         if ($check) {
-            debug()->trace(__('__get_permission %d', $id), Manager::getPermissonsByUserId($id));
-            $permission=Manager::getPermissonsByUserId($id);
-            if ($permission) {
-                $this->setPermission(new Permission($permission));
-            } else {
-                $this->setPermission(new Permission);
-            }
+            $this->refreshPermission($id);
         }
         return $check;
+    }
+
+    public function getId()
+    {
+        if ($this->hasPermission('admin:user.simulate') && cookie()->has(User::simulateUserToken)) {
+            $userId=cookie()->get(User::simulateUserToken,$this->id);
+            debug()->trace(__( 'user_simulated  %d --> %d',$this->id ,$userId));
+            if ($this->simulate != $userId){
+                $this->refreshPermission($userId);
+            }
+            return $userId;
+        }
+        return $this->simulate ?? $this->id;
     }
 
     public function sign(int $id, bool $remember)
@@ -33,5 +42,16 @@ class User extends Visitor
         $this->refresh($id, $token);
         Context::getInstance()->cookieVisitor($this)->expire($token_expire)->session(!$remember)->set();
         return $this;
+    }
+
+    protected function refreshPermission(int $id){
+        $permission=Manager::getPermissonsByUserId($id);
+        debug()->trace(__('refresh_permission_for_user %d', $id), $permission);
+        $this->simulate = $id;
+        if ($permission) {
+            $this->setPermission(new Permission($permission));
+        } else {
+            $this->setPermission(new Permission);
+        }
     }
 }
