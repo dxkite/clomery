ffa7f0eec366cdbaa98f4b588ddc45eace8233be
DXkite <dxkite@163.com>
2018-02-20 17:25:45 +0800

    添加：公共子模块

---
+++ public/dev.php
@@ -0,0 +1,18 @@
+<?php
+    // 应用所在目录
+    define('APP_DIR', __DIR__.'/../app');
+    // 日志所在目录
+    define('APP_LOG', APP_DIR.'/data/logs');
+    // 系统所在目录
+    define('SYSTEM',__DIR__.'/../suda/system');
+    // 网站根目录位置
+    define('APP_PUBLIC',__DIR__);
+    // 开发者模式
+    define('DEBUG',true);
+    // 日志纪录等级
+    define('LOG_LEVEL', 'trace');
+    // 输出日志详细信息到json文档
+    define('LOG_JSON',false);
+    // 输出详细信息添加到日志末尾
+    define('LOG_FILE_APPEND',true);
+    require_once SYSTEM.'/suda.php';
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php  
+     // 应用所在目录
+    define('APP_DIR', __DIR__.'/../app');
+    // 日志所在目录
+    define('APP_LOG', APP_DIR.'/data/logs');
+    // 系统所在目录
+    define('SYSTEM', __DIR__.'/../suda/system');
+    // 网站根目录位置
+    define('APP_PUBLIC', __DIR__);
+    // 关闭系统管理模块
+    define('DISABLE_MODULES', 'suda');
+    // 开发者关闭模式
+    define('DEBUG', false);
+    // 错误等级
+    define('LOG_LEVEL', 'error');
+    // 输出日志详细信息到json文档
+    define('LOG_JSON',false);
+    // 输出详细信息添加到日志末尾
+    define('LOG_FILE_APPEND',true);
+    require_once SYSTEM.'/suda.php';
