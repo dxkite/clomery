91f938565e0b013d3a92e611739a45f2e83ed109
DXkite <dxkite@163.com>
2018-02-22 20:18:14 +0800

    添加：广告信息编辑

--- app/modules/dxkite-android-message/share/Message.php
+++ app/modules/dxkite-android-message/share/Message.php
@@ -2,24 +2,27 @@
 namespace dxkite\android\message;
 
 use dxkite\support\proxy\ProxyObject;
+use  dxkite\support\file\Media;
+use dxkite\support\file\File;
 
 class Message extends ProxyObject
 {
-    public function pull()
+    public static function pull()
     {
-        $message = setting('androidMessage',false);
-        if ($message && setting('androidMessageEnable',false)){
+        $message = setting('androidMessage', false);
+        if ($message && setting('androidMessageEnable', false)) {
             return $message;
         }
         return false;
     }
 
-    public function pullAd()
+    public static function pullAd()
     {
-        return setting('androidAds',false);
+        return setting('androidAds', false);
     }
 
-    public function editPull(string $message,int $time=10000,string $url=null,string $color='#222222',string $bgColor='#EEEEEE'){
+    public static function editPull(string $message, int $time=10000, string $url=null, string $color='#222222', string $bgColor='#EEEEEE')
+    {
         $message = [
             'message' => $message,
             'url'=>$url,
@@ -27,19 +30,32 @@ class Message extends ProxyObject
             'color'=>$color,
             'backgroundColor'=> $bgColor,
             'touchable' => !empty($url),
+            'create'=>time(),
         ];
-        return setting_val('androidMessage',$message);
+        return setting_set('androidMessage', $message);
     }
 
-    public function editAds(string $image,string $url){
-        $ads=[
-            'image'=>$image,
-            'url'=>$url,
-        ];
-        return setting_val('androidAds',$ads);
+    public static function editAds(File $image, string $url)
+    {
+        if ($old=setting('androidAds')) {
+            if (isset($old['imageId'])) {
+                Media::delete($old['imageId']);
+            }
+        }
+        $upload=Media::saveFile($image);
+        if ($upload->getId()>0) {
+            $ads=[
+                'image'=>  u('support:upload', ['id'=>$upload->getId()]),
+                'imageId'=> $upload->getId(),
+                'url'=>$url,
+            ];
+            return setting_set('androidAds', $ads);
+        }
+        return false;
     }
 
-    public function enableMessage(bool $enable=true) {
-        return setting_val('androidMessageEnable',$enable);
+    public static function enableMessage(bool $enable=true)
+    {
+        return setting_set('androidMessageEnable', $enable);
     }
 }
--- app/modules/dxkite-android-message/src/setting/ManagerResponse.php
+++ app/modules/dxkite-android-message/src/setting/ManagerResponse.php
@@ -2,13 +2,38 @@
 namespace dxkite\android\message\response\setting;
 
 use dxkite\support\visitor\Context;
-use dxkite\support\template\Manager;
 use dxkite\user\table\UserTable;
+use  dxkite\android\message\Message;
+use dxkite\support\file\File;
 
 class ManagerResponse extends \dxkite\support\setting\Response
 {
     public function onAdminView($view, $context)
     {
+        if (request()->hasPost()) {
+            if (request()->get()->action == 'color') {
+                Message::editPull(
+                    request()->post('message'),
+                    request()->post('time', 5000),
+                    request()->post('url'),
+                    request()->post('color'),
+                    request()->post('bgcolor')
+                );
+                Message:: enableMessage(request()->post('show', true));
+                $this->refresh();
+                return false;
+            } elseif (request()->get()->action == 'image') {
+                Message::editAds(
+                    File::createFromPost('image'),
+                    request()->post('url')
+                );
+            }
+        }
+        $message = setting('androidMessage');
+        if ($message) {
+            $view->set('message', $message);
+        }
+        $view->set('show', setting('androidMessageEnable'));
         return true;
     }
 
