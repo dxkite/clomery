1237eafba6ea3c149bf217b5a625bef641404ddb
dxkite <DXkite@163.com>
2019-07-07 16:56:15 +0800

    添加：初始化提交

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+define('SUDA_APP', __DIR__.'/../app');
+define('SUDA_DATA', __DIR__.'/../data');
+define('SUDA_SYSTEM', __DIR__.'/../vendor/dxkite/suda/suda');
+define('SUDA_PUBLIC', __DIR__);
+define('SUDA_DEBUG', true);
+define('SUDA_DEBUG_LEVEL', 'info');
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once SUDA_SYSTEM.'/loader/main.php';
+
+
