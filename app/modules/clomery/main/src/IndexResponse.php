b48b18973410a538187f76de0c9792ac1486cfa8
dxkite <DXkite@163.com>
2019-07-19 08:33:00 +0800

    添加：首页代码

--- app/modules/clomery/main/src/IndexResponse.php
+++ app/modules/clomery/main/src/IndexResponse.php
@@ -1,10 +1,26 @@
 <?php
 
 
-namespace clomery\article\response;
+namespace clomery\main\response;
 
 
-class IndexResponse
+
+use suda\application\Application;
+use suda\application\processor\RequestProcessor;
+use suda\framework\Request;
+use suda\framework\Response;
+
+class IndexResponse implements RequestProcessor
 {
 
+    /**
+     * @param Application $application
+     * @param Request $request
+     * @param Response $response
+     * @return mixed
+     */
+    public function onRequest(Application $application, Request $request, Response $response)
+    {
+        return $application->getTemplate('index', $request);
+    }
 }
--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ define('SUDA_DATA', __DIR__.'/../data');
 define('SUDA_SYSTEM', __DIR__.'/../vendor/dxkite/suda/suda');
 define('SUDA_PUBLIC', __DIR__);
 define('SUDA_DEBUG', true);
-define('SUDA_DEBUG_LEVEL', 'info');
+define('SUDA_DEBUG_LEVEL', 'debug');
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once SUDA_SYSTEM.'/loader/main.php';
